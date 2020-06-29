426840fd1ff725fba95941797a19981425b73490
tzachbon <tzachb@wix.com>
2020-06-29 19:43:17 +0300

    fix: storing in storage while setting new frame as active

--- src/utils/manager/frame.ts
+++ src/utils/manager/frame.ts
@@ -1,25 +1,18 @@
 import { Frame, ACTIONS } from "../../models/frame";
 
-
 export const setFrame = (frame: Frame, addUrl = true) => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     const activeTab = tabs[0];
     const url = activeTab.url;
 
-    chrome.tabs.create(
-      {
-        url: chrome.runtime.getURL("manager.html"),
-        active: false,
+    chrome.storage.sync.set({
+      [ACTIONS.FRAME]: {
+        frame,
+        url: addUrl ? url : null,
       },
-      (managerTab) => {
-        chrome.storage.sync.set({
-          [ACTIONS.FRAME]: {
-            frame,
-            url: addUrl ? url : null,
-          },
-        });
-      }
-    );
+    });
+    chrome.tabs.create({
+      url: chrome.runtime.getURL("manager.html"),
+    });
   });
 };
-
