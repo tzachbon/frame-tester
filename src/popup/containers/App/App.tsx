ae0ec589ba6d281d5f954a75e895cf0c6d776a21
tzachbon <tzachb@wix.com>
2020-06-29 19:28:38 +0300

    fix: get assets

--- src/popup/containers/App/App.tsx
+++ src/popup/containers/App/App.tsx
@@ -4,10 +4,14 @@ import { FRAMES } from "../../../models/frame";
 import { setFrame } from "../../../utils/manager/frame";
 import * as styles from "./style.scss";
 
+const getBackgroundImage = () =>
+  chrome.extension.getURL("assets/images/popup-background.png");
+
 interface AppProps {}
 
 const App: React.FC<AppProps> = () => {
   const [currentFrame, setCurrentFrame] = React.useState<string>(FRAMES.SAFARI);
+  const bgImage = React.useMemo(() => getBackgroundImage(), []);
 
   const handleSubmit = () => {
     if (
@@ -22,7 +26,10 @@ const App: React.FC<AppProps> = () => {
   const handleChange = ({ target: { value } }) => setCurrentFrame(value);
 
   return (
-    <div className={styles.popupContainer}>
+    <div
+      className={styles.popupContainer}
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       <div className='background-dots-layer'></div>
       <h1>Frame Tester</h1>
       <br />
