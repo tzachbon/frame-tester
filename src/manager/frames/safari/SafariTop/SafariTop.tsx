dc9f1e9642c00222276443e09d18725babc6c5ba
AdiAharonov <adi.ahronov@gmail.com>
2020-07-10 13:32:59 +0300

    upload frame

--- src/manager/frames/safari/SafariTop/SafariTop.tsx
+++ src/manager/frames/safari/SafariTop/SafariTop.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react'
+import * as styles from "../style.scss";
+
+
+const aa = chrome.extension.getURL("assets/safariFrame/AA.svg");
+const reload = chrome.extension.getURL("assets/safariFrame/reload.svg");
 
 const SafariTop = () => {
   return (
-    <div >
-      <h1>hhhhhh</h1>
+    <div className={styles.safariTop}>
+      <div>
+      <input type="text" />
+      <img src={aa} className={styles.aA} />
+      <h2>Google.com</h2>
+      <img src={reload} className={styles.reload} />
+      </div>
     </div>
   )
 }
--- src/manager/frames/safari/index.tsx
+++ src/manager/frames/safari/index.tsx
@@ -1,14 +1,15 @@
 import * as React from "react";
 import { FrameProps } from "../../../models/frame";
-// import  SafariTop  from "./SafariTop/index";
+import  SafariTop  from "./SafariTop/index";
+import  SafariBottom  from "./SafariBottom/index";
 // import SafariFrame from "manager/components/App/frames/SafariFrame";
 
 const Safari: React.FC<FrameProps> = ({ url, Iframe, frame }) => {
   return (
     <div>
-      <h1>{frame}</h1>
-      {/* <SafariTop /> */}
+      <SafariTop />
       <Iframe />
+      <SafariBottom />
     </div>
   );
 };
