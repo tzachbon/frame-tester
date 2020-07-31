ec9804d13a528fafdbe5cd504d0127bf3af81446
tzachbon <tzachb@wix.com>
2020-07-31 12:02:59 +0300

    fix(safari-frame): ui changed + show origin url

--- src/manager/frames/safari/SafariTop/SafariTop.tsx
+++ src/manager/frames/safari/SafariTop/SafariTop.tsx
@@ -1,21 +1,24 @@
-import * as React from 'react'
+import * as React from "react";
 import * as styles from "../style.scss";
 
 
 const aa = chrome.extension.getURL("assets/safariFrame/AA.svg");
 const reload = chrome.extension.getURL("assets/safariFrame/reload.svg");
 
-const SafariTop = () => {
-  return (
-    <div className={styles.safariTop}>
-      <div>
-      <input type="text" />
-      <img src={aa} className={styles.aA} />
-      <h2>Google.com</h2>
-      <img src={reload} className={styles.reload} />
-      </div>
-    </div>
-  )
+interface Props {
+  url: string;
 }
 
-export default SafariTop
+const SafariTop: React.FC<Props> = ({ url }) => {
+  return (
+    <div className={styles.safariTop}>
+      <div className={styles.safariTopContainer}>
+        <img src={aa} className={styles.textIcon} />
+        <h2>{url}</h2>
+        <img src={reload} className={styles.reloadIcon} />
+      </div>
+    </div>
+  );
+};
+
+export default SafariTop;
--- src/manager/frames/safari/index.tsx
+++ src/manager/frames/safari/index.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
-import * as styles from './style.scss';
+import * as styles from "./style.scss";
 import { FrameProps } from "../../../models/frame";
-import  SafariTop  from "./SafariTop/index";
-import  SafariBottom  from "./SafariBottom/index";
+import SafariTop from "./SafariTop/index";
+import SafariBottom from "./SafariBottom/index";
 
 const Safari: React.FC<FrameProps> = ({ url, Iframe, frame }) => {
   return (
     <div className={styles.root}>
-      <SafariTop />
+      <SafariTop url={url} />
       <Iframe className={styles.Iframe} />
       <SafariBottom />
     </div>
